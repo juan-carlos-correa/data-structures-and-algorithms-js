1df288d71cad99ff26542fe90c91300e54bcf96a
Juan Correa <juan.correa.herrera@gmail.com>
2025-04-28 10:16:02 -0600

    remove isAnagram implementation and related files

--- src/data-structures/hashmap/is-anagram/src/main.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * Returns true if str1 and str2 are anagrams
- * @param {string} str1
- * @param {string} str2
- * @returns {boolean}
- */
-export function isAnagram(str1, str2) {
-  // Your solution here
-}
